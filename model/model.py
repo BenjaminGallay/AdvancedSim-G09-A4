67116589aaa06f5ebfd13d3902541314785b61b0
Groumphh <groumphh38@gmail.com>
2026-03-25 15:10:15 +0100

    Computation of edge breakdown probability and graph display

--- model/model.py
+++ model/model.py
@@ -1,12 +1,11 @@
 import os
 import time
 
-import analytical_recorder
+import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
 import networkx as nx
+import numpy as np
 import pandas as pd
-
-from model import analytical_recorder
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 file_name = os.path.join(BASE_DIR, "data", "roads.csv")
@@ -23,43 +22,12 @@ def generate_graph():
     df = pd.read_csv(file_name)
 
     # a list of names of roads to be generated
-    # TODO You can also read in the road column to generate this list automatically
-    roads = [
-        "N1",
-        "N101",
-        "N102",
-        "N103",
-        "N104",
-        "N105",
-        "N106",
-        "N107",
-        "N108",
-        "N109",
-        "N110",
-        "N111",
-        "N112",
-        "N119",
-        "N120",
-        "N123",
-        "N128",
-        "N129",
-        "N2",
-        "N203",
-        "N204",
-        "N205",
-        "N206",
-        "N207",
-        "N208",
-        "N209",
-        "N210",
-    ]
-
     all_roads = df["road"].unique().tolist()
     n_roads = [road for road in all_roads if road[0] == "N"]
-    # roads = ["N1", "N102", "N103"]
-    speed = 48 * 1000 / 60
+    n12_roads = [road for road in all_roads if road[:2] == "N1"] + [
+        road for road in all_roads if road[:2] == "N2"
+    ]
     graph = nx.Graph()
-    breakdown_probabilities = [0.05, 0.1, 0.2, 0.4]
 
     df_objects_all = []
     for road in all_roads:
@@ -75,8 +43,7 @@ def generate_graph():
     # Store the information about the start of the segment of road the program is currently following
     current_edge_start = {"road": None, "id": None}
     current_edge_weight = 0
-    current_edge_id_list = []
-    current_edge_mean_bridge_delay = 0
+    current_edge_bridge_conditions = {0: 0, 1: 0, 2: 0, 3: 0}
 
     for df in df_objects_all:
         for _, row in df.iterrows():  # index, row in ...
@@ -103,12 +70,13 @@ def generate_graph():
                         current_edge_start["id"],
                         row["id"],
                         weight=current_edge_weight,
-                        mean_delay=current_edge_mean_bridge_delay,
+                        shutdown_probability=get_edge_shutdown_probability(
+                            current_edge_bridge_conditions
+                        ),
                     )
                 current_edge_start = {"road": row["road"], "id": row["id"]}
                 current_edge_weight = 0
-                current_edge_id_list = []
-                current_edge_mean_bridge_delay = 0
+                current_edge_bridge_conditions = {0: 0, 1: 0, 2: 0, 3: 0}
 
             elif model_type == "sink":
                 # We add a node corresponding to the element and link it to the previous node if they are on the same road
@@ -124,12 +92,13 @@ def generate_graph():
                         current_edge_start["id"],
                         row["id"],
                         weight=current_edge_weight,
-                        mean_delay=current_edge_mean_bridge_delay,
+                        shutdown_probability=get_edge_shutdown_probability(
+                            current_edge_bridge_conditions
+                        ),
                     )
                 current_edge_start = {"road": row["road"], "id": row["id"]}
                 current_edge_weight = 0
-                current_edge_id_list = []
-                current_edge_mean_bridge_delay = 0
+                current_edge_bridge_conditions = {0: 0, 1: 0, 2: 0, 3: 0}
 
             elif model_type == "sourcesink":
                 # We add a node corresponding to the element and link it to the previous node if they are on the same road
@@ -145,23 +114,20 @@ def generate_graph():
                         current_edge_start["id"],
                         row["id"],
                         weight=current_edge_weight,
-                        mean_delay=current_edge_mean_bridge_delay,
+                        shutdown_probability=get_edge_shutdown_probability(
+                            current_edge_bridge_conditions
+                        ),
                     )
                 current_edge_start = {"road": row["road"], "id": row["id"]}
                 current_edge_weight = 0
-                current_edge_id_list = []
-                current_edge_mean_bridge_delay = 0
+                current_edge_bridge_conditions = {0: 0, 1: 0, 2: 0, 3: 0}
 
             elif model_type == "bridge":
                 current_edge_weight += row["length"]
-                current_edge_id_list.append(row["id"])
-                current_edge_mean_bridge_delay += (
-                    analytical_recorder.compute_bridge_mean_delay(row["length"])
-                ) * breakdown_probabilities[int(row["condition"])]
+                current_edge_bridge_conditions[row["condition"]] += 1
 
             elif model_type == "link":
                 current_edge_weight += row["length"]
-                current_edge_id_list.append(row["id"])
 
             elif model_type == "intersection":
                 # Intersection elements are stored in multiple roads, it is necessary to check wether it is already in the graph or not.
@@ -182,22 +148,28 @@ def generate_graph():
                         current_edge_start["id"],
                         row["id"],
                         weight=current_edge_weight,
-                        mean_delay=current_edge_mean_bridge_delay,
+                        shutdown_probability=get_edge_shutdown_probability(
+                            current_edge_bridge_conditions
+                        ),
                     )
                 current_edge_start = {"road": row["road"], "id": row["id"]}
                 current_edge_weight = 0
-                current_edge_id_list = []
-                current_edge_mean_bridge_delay = 0
+                current_edge_bridge_conditions = {0: 0, 1: 0, 2: 0, 3: 0}
+
     print("delta", time.time() - start)
-    print(len(list(graph.nodes)))
-    draw_graph(graph)
-    graph = prune_graph(graph, "weight")
-    print(len(list(graph.nodes)))
+    # graph = prune_graph(graph, "weight")
     draw_graph(graph)
     return graph
 
 
-def prune_graph(graph, weight_attr="weight"):
+def get_edge_shutdown_probability(dict):
+    working_edge_probability = 1
+    for i in dict:
+        working_edge_probability *= (1 - ((1 / 10) ** (4 - i))) ** dict[i]
+    return 1 - working_edge_probability
+
+
+def prune_graph(graph, weight_attr="weight", prob_attr="shutdown_probability"):
     # We iterate until no more degree-2 nodes exist
     changed = True
     while changed:
@@ -214,12 +186,16 @@ def prune_graph(graph, weight_attr="weight"):
             w2 = graph[node][v].get(weight_attr, 1)
             new_weight = w1 + w2
 
+            p1 = graph[node][u].get(prob_attr, 1)
+            p2 = graph[node][v].get(prob_attr, 1)
+            new_prob = 1 - ((1 - p1) * (1 - p2))
+
             # If edge already exists, sum weights
             if graph.has_edge(u, v):
                 existing_weight = graph[u][v].get(weight_attr, 1)
                 graph[u][v][weight_attr] = existing_weight + new_weight
             else:
-                graph.add_edge(u, v, **{weight_attr: new_weight})
+                graph.add_edge(u, v, **{weight_attr: new_weight, prob_attr: new_prob})
 
             graph.remove_node(node)
             changed = True
@@ -229,13 +205,17 @@ def prune_graph(graph, weight_attr="weight"):
 
 # draws the graph of the road network
 def draw_graph(graph):
+    fig, ax = plt.subplots(figsize=(10, 10))
+
+    # Positions
     pos = {n: (d["lon"], d["lat"]) for n, d in graph.nodes(data=True)}
 
-    # Assign colors and sizes to nodes depending on type of road
+    # Node styling
     colors, sizes = [], []
-    for n, d in graph.nodes(data=True):
-        set = d.get("road")
-        road_types = [road[0] for road in set]
+    for _, d in graph.nodes(data=True):
+        roads = d.get("road", [])
+        road_types = [road[0] for road in roads]
+
         if "N" in road_types:
             colors.append("purple")
             sizes.append(20)
@@ -246,8 +226,52 @@ def draw_graph(graph):
             colors.append("purple")
             sizes.append(5)
 
-    nx.draw_networkx_nodes(graph, pos, node_size=sizes, node_color=colors)
-    nx.draw_networkx_edges(graph, pos, edge_color="turquoise", arrows=False)
+    # Edge data
+    edges = list(graph.edges(data=True))
+    probs = np.array([d.get("shutdown_probability", 0) for _, _, d in edges])
+    print([float(np.log10(prob)) for prob in probs])
+
+    # --- LOG SCALING ---
+    eps = 1e-3
+    probs_safe = np.clip(probs, eps, 1)
+
+    log_probs = np.log10(probs_safe)
+
+    # Normalize log values to [0, 1]
+    log_min = np.log10(eps)
+    log_max = 0  # log10(1)
+    probs_scaled = (log_probs - log_min) / (log_max - log_min)
+
+    # --- DRAW ---
+    nx.draw_networkx_nodes(graph, pos, node_size=sizes, node_color=colors, ax=ax)
+
+    nx.draw_networkx_edges(
+        graph,
+        pos,
+        edgelist=[(u, v) for u, v, _ in edges],
+        edge_color=probs_scaled,
+        edge_cmap=plt.cm.RdYlGn_r,  # green → red
+        edge_vmin=0,
+        edge_vmax=1,
+        width=1.5,
+        alpha=0.9,
+        arrows=False,
+        ax=ax,
+    )
+
+    # --- COLORBAR (log scale labels!) ---
+    norm = mcolors.LogNorm(vmin=eps, vmax=1)
+    sm = plt.cm.ScalarMappable(cmap=plt.cm.RdYlGn_r, norm=norm)
+    sm.set_array([])
+
+    cbar = fig.colorbar(sm, ax=ax)
+    cbar.set_label("Shutdown Probability of Edges (log scale)")
+
+    # GIS-style cleanup
+    ax.set_aspect("equal")
+    ax.set_axis_off()
+    plt.tight_layout()
+
     plt.show()
     return
 
